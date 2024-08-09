ad7ed2e4020912b6038f3128d887bea07a7fa090
Sebastian <spartan89632@hotmail.com>
2024-08-08 19:54:15 -0500

    Agregar archivos de proyecto.

---
+++ PhoneBookApp/Data/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using PhoneBookApp.Models;
+
+namespace PhoneBookApp.Data
+{
+    internal class DatabaseManager: DbContext
+    {
+
+        public readonly string dbConnection = ConfigurationManager.AppSettings.Get("SQLConnection");
+
+        DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(dbConnection);
+        }
+
+    }
+}
